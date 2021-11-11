63b9c002fc629af7d25a9757b0e09abb55f7949a
123mjb <micahbutt@outlook.com>
2021-11-11 13:42:29 +0000

    update

--- randmovement.py
+++ randmovement.py
@@ -1,4 +1,38 @@
+import pygame, math, sys
 import random as r
-import pygame
+from pygame.locals import *
 
-direction = r.randrange(1, 360)
+directionx = r.randrange(-5, 5)
+directiony = r.randrange(-5, 5)
+FPS = 60
+FramePerSec = pygame.time.Clock()
+DISPLAYSURF = pygame.display.set_mode((800,600))
+
+pygame.init()
+
+class Player2(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load("Player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (560, 520)
+       
+    def move(self):
+        self.rect.move_ip(directionx, directiony)
+        
+        
+
+while True:
+      
+    #Cycles through all events occuring  
+    for event in pygame.event.get(): 
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    #Moves and Re-draws all Sprites
+    Player2.move()
+    DISPLAYSURF.blit(Player2.image, Player2.rect)
+        
+    pygame.display.update()
+    FramePerSec.tick(FPS)
