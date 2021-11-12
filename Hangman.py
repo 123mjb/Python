da9cfa6ca0ea8c4d1e14b4c90fe0c008ae4119f4
123mjb <micahbutt@outlook.com>
2021-11-12 13:29:53 +0000

    update to hangman

--- Hangman.py
+++ Hangman.py
@@ -1,8 +1,20 @@
 import random as r
 
+wordlength = 0
+wrdplce = 0
 number = 58110
 file = open("EveryWordInTheEnglishLanguage.txt", "r")
 string = file.read()
 list = string.split("\n")
 word = list[r.randint(0, number)]
-print(word)
+for i in word:
+    wordlength += 1
+prnt = "_" * wordlength
+while prnt != word:
+    print(prnt)
+    letter = input()
+    for y in word:
+        if letter == y:
+            prnt = prnt.replace(wrdplce, word[wrdplce])
+        wrdplce += 1
+
