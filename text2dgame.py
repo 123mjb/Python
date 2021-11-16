bb4f0cf1318a5b332fd8767cf0ae8145ba506560
123mjb <micahbutt@outlook.com>
2021-11-16 18:21:05 +0000

    Fixed 2d game

--- text2dgame.py
+++ text2dgame.py
@@ -1,68 +1,68 @@
 level1=[
-    " ■ ", " ■ ", " ■ ", " ■ ", " ■ ", " ■ ", " ■\n",
-    " ■ ", "⯐", "  ", "  ", "  ", "  ", " ■\n",
-    " ■ ", "  ", "  ", "  ", "  ", "  ", " ■\n",
-    " ■ ", "  ", "  ", " ⛾ ", "  ", "  ", " ■\n",
-    " ■ ", "  ", "  ", "  ", "  ", "  ", " ■\n",
-    " ■ ", "  ", "  ", "  ", "  ", "  ", " ■\n",
-    " ■ ", " ■ ", " ■ ", " ■ ", " ■ ", " ■ ", " ■\n",
+    "■ ", "■ ", "■ ", "■ ", "■ ", "■ ", "■\n",
+    "■ ", "⯐ ", "  ", "  ", "  ", "  ", "■\n",
+    "■ ", "  ", "  ", "  ", "  ", "  ", "■\n",
+    "■ ", "  ", "  ", "⛾ ", "  ", "  ", "■\n",
+    "■ ", "  ", "  ", "  ", "  ", "  ", "■\n",
+    "■ ", "  ", "  ", "  ", "  ", "  ", "■\n",
+    "■ ", "■ ", "■ ", "■ ", "■ ", "■ ", "■\n",
 ]
 playerpos=8
-moveblockpos=25
+moveblockpos=24
 pront=""
 def move():
     global playerpos
     inpt=input()
     if inpt=="w":
-        if level1[playerpos-7]!=" ■ " and level1[playerpos-7]!=" ■\n":
-            if level1[playerpos-7]==" ⛾ ":
+        if level1[playerpos-7]!="■ " and level1[playerpos-7]!="■\n":
+            if level1[playerpos-7]=="⛾ ":
                 if moveblock(-7):
                     level1[playerpos]="  "
                     playerpos-=7
-                    level1[playerpos]="⯐"
+                    level1[playerpos]="⯐ "
             else:
                 level1[playerpos]="  "
                 playerpos-=7
-                level1[playerpos]="⯐"           
+                level1[playerpos]="⯐ "           
     elif inpt == "a":
-        if level1[playerpos-1]!=" ■ " and level1[playerpos-1]!=" ■\n":
-            if level1[playerpos-1]==" ⛾ ":
+        if level1[playerpos-1]!="■ " and level1[playerpos-1]!="■\n":
+            if level1[playerpos-1]=="⛾ ":
                 if moveblock(-1):
                     level1[playerpos]="  "
                     playerpos-=1
-                    level1[playerpos]="⯐"
+                    level1[playerpos]="⯐ "
             else:
                 level1[playerpos]="  "
                 playerpos-=1
-                level1[playerpos]="⯐" 
+                level1[playerpos]="⯐ " 
     elif inpt == "s":
-        if level1[playerpos+7]!=" ■ " and level1[playerpos+7]!=" ■\n":
-            if level1[playerpos+7]==" ⛾ ":
+        if level1[playerpos+7]!="■ " and level1[playerpos+7]!="■\n":
+            if level1[playerpos+7]=="⛾ ":
                 if moveblock(+7):
                     level1[playerpos]="  "
                     playerpos+=7
-                    level1[playerpos]="⯐"
+                    level1[playerpos]="⯐ "
             else:
                 level1[playerpos]="  "
                 playerpos+=7
-                level1[playerpos]="⯐" 
+                level1[playerpos]="⯐ " 
     elif inpt == "d":
-        if level1[playerpos+1]!=" ■ " and level1[playerpos+1]!=" ■\n":
-            if level1[playerpos+1]==" ⛾ ":
+        if level1[playerpos+1]!="■ " and level1[playerpos+1]!="■\n":
+            if level1[playerpos+1]=="⛾ ":
                 if moveblock(+1):
                     level1[playerpos]="  "
                     playerpos+=1
-                    level1[playerpos]="⯐"
+                    level1[playerpos]="⯐ "
             else:
                 level1[playerpos]="  "
                 playerpos+=1
-                level1[playerpos]="⯐"      
+                level1[playerpos]="⯐ "      
 def moveblock(direct):
     global moveblockpos
-    if level1[moveblockpos+direct]!=" ■ " and level1[moveblockpos+direct]!=" ■\n":
+    if level1[moveblockpos+direct]!="■ " and level1[moveblockpos+direct]!="■\n":
         level1[moveblockpos]="  "
         moveblockpos+=direct
-        level1[moveblockpos]=" ⛾ "
+        level1[moveblockpos]="⛾ "
         answer = True
         print(direct)
     else:
