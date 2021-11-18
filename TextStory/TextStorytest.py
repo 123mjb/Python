d6b09861eae6c6d248061dd25645de0161a2ddb5
123mjb <micahbutt@outlook.com>
2021-11-18 08:24:32 +0000

    Updates

--- TextStory/TextStorytest.py
+++ TextStory/TextStorytest.py
@@ -1,2 +1,8 @@
 import TextstoryBasis as tsb
-tsb.printvalues("Health", 100, "Damage", 20, "Defence", 50, "Magic", 0)
+names = ["Health", "Damage", "Defence", "Magic"]
+Values = [100, 20, 50, 0]
+upgrade1vals = [10, 30, 5, 0] # warrior
+upgrade2vals = [10, -5, 5, 20] # wizard
+upgrade3vals = [40, 5, 5, 5] # healer
+upgrade4vals = [15, 15, 30, 0] # tank
+tsb.printvalues(names, Values)
--- TextStory/TextstoryBasis.py
+++ TextStory/TextstoryBasis.py
@@ -1,5 +1,6 @@
-def printvalues(val1, value1, val2, value2, val3, value3, val4, value4, ):
-    lst = [val1, val2, val3, val4]
-    lst2 = [value1, value2, value3, value4]
+def printvalues(lst, lst2):
     for i in range(0, 4):
         print(lst[i], "is", lst2[i])
+def addvalues(values, addlst):
+    for i in range(0, 4):
+        values[i] += addlst[i]
