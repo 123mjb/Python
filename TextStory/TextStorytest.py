b2da92abd3959f9b790073a84181b1130909a117
123mjb <micahbutt@outlook.com>
2021-11-18 21:10:23 +0000

    Updates

--- TextStory/TextStorytest.py
+++ TextStory/TextStorytest.py
@@ -1,8 +1,11 @@
 import TextstoryBasis as tsb
 names = ["Health", "Damage", "Defence", "Magic"]
 Values = [100, 20, 50, 0]
-upgrade1vals = [10, 30, 5, 0] # warrior
+upgrade1vals = [10, 30, 5, 0] # beserker
 upgrade2vals = [10, -5, 5, 20] # wizard
 upgrade3vals = [40, 5, 5, 5] # healer
 upgrade4vals = [15, 15, 30, 0] # tank
-tsb.printvalues(names, Values)
+while True:
+    tsb.printvalues(names, Values)
+    Values = tsb.chooseupgrade(Values, upgrade1vals, upgrade2vals, upgrade3vals, upgrade4vals)
+
--- TextStory/TextstoryBasis.py
+++ TextStory/TextstoryBasis.py
@@ -4,3 +4,15 @@ def printvalues(lst, lst2):
 def addvalues(values, addlst):
     for i in range(0, 4):
         values[i] += addlst[i]
+def chooseupgrade(values, b, w, h, t):
+    inpt = input()
+    for i in range(0, 4):
+        if inpt == "b":
+            values[i] += b[i]
+        elif inpt == "w":
+            values[i] += w[i]
+        elif inpt == "h":
+            values[i] += h[i]
+        elif inpt == "t":
+            values[i] += t[i]
+    return values
