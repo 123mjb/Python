255180e262f80ab94e74522987019b880697511b
123mjb <micahbutt@outlook.com>
2021-11-22 14:16:14 +0000

    Circle creator fixed

--- CircleCreator.py
+++ CircleCreator.py
@@ -3,6 +3,8 @@ from pygame.locals import *
 
 pygame.init()
 
+FPS = 60
+FramePerSec = pygame.time.Clock()
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 400
 BLACK = (0, 0, 0)
@@ -13,5 +15,13 @@ DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Circles")
 
 while True:
-    for i in range(5, 395, 5):
-        pygame.draw.circle(DISPLAYSURF, BLACK, (i, 5), 5, 1)
+    for event in pygame.event.get():    
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    for j in range(5, 605, 10):
+        for i in range(5, 805, 10):
+            pygame.draw.circle(DISPLAYSURF, BLACK, (i, j), 5, 1)  
+
+    pygame.display.update()
+    FramePerSec.tick(FPS)
