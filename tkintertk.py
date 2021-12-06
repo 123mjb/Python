93576d2cb1a77dfefe3645ad15baaedd1f3971ca
123mjb <micahbutt@outlook.com>
2021-12-06 14:14:00 +0000

    tkinter game

--- tkintertk.py
+++ tkintertk.py
@@ -10,12 +10,18 @@ def openNewWindow():
     frm2.grid()
     newWindow.title("New Window")
     newWindow.geometry("200x200")
+    def getSquareRoot ():  
+        x1 = entry1.get()
+
+        label1 = tk.Label(root, text= float(x1)**0.5)
+        canvas1.create_window(200, 230, window=label1)
     def multiplication():
         num1, num2 = r.randrange(1, 10), r.randrange(1, 10)
         txt=num1,"X",num2
         ttk.Label(frm2, text=txt).grid(column=0, row=0)
-        entry1 = tk.Entry(frm2) 
+        entry1 = ttk.Entry(frm2) 
         newWindow.create_window(200, 140, window=entry1)
+        button1 = ttk.Button(text='Get the Square Root', command=getSquareRoot)
     Label(newWindow,
           multiplication()).pack()
 ttk.Label(frm, text="Hello").grid(column=0, row=0)
