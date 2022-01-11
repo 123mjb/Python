dfde499da9997f43dab92cfd113f68198d615119
123mjb <micahbutt@outlook.com>
2022-01-11 14:10:48 +0000

    stuFF

---
+++ autogui.py
@@ -0,0 +1,8 @@
+# importing modules
+import pyautogui
+pyautogui.FAILSAFE=False
+# returns a size object with
+# width and height of the screen
+print(pyautogui.size())
+pyautogui.click(x=1919, y=1079, clicks=1, interval=0, button='left')
+
