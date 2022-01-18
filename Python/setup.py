b33fbcf1cf8d04db4d92c9caedb6b58263ce30fc
123mjb <micahbutt@outlook.com>
2022-01-18 14:11:30 +0000

    sorting out

--- Python/setup.py
+++ Python/setup.py
@@ -1,5 +1,5 @@
-import subprocess, os, json
-TARGET = "C:\\Users\\19ebuttmi\\AppData\\Roaming\\Code\\User\\settings.json" if os.path.isfile("C:\\Users\\19ebuttmi\\AppData\\Roaming\\Code\\User\\settings.json") else "C:\\Users\\19ebuttmi.STRS\\AppData\\Roaming\\Code\\User\\settings.json"
+import subprocess, os, json, getpass
+TARGET = "C:\\Users\\{}\\AppData\\Roaming\\Code\\User\\settings.json".format(getpass.getuser()) # if os.path.isfile("C:\\Users\\19njonebe\\AppData\\Roaming\\Code\\User\\settings.json") else "C:\\Users\\19njonebe.STRS\\AppData\\Roaming\\Code\\User\\settings.json"
 json.dump(json.loads("".join(open(TARGET).readlines()).replace(",\n}", "}")) | {"http.proxyStrictSSL": False, "git.enabled": True, "git.path": "C:\\Program Files\\GitHub Desktop\\resources\\app\\git\\cmd\\git.exe"}, open(TARGET, "w"))
 (subprocess.call(["pip", "install", "cs50", "flask-session"]), [os.system("code --install-extension " + i) for i in ["PKief.material-icon-theme", "eamodio.gitlens", "github.github-vscode-theme"]])
 json.dump(json.loads("".join(open(TARGET).readlines()).replace(",\n}", "}")) | {"workbench.colorTheme": "GitHub Dark", "workbench.iconTheme": "material-icon-theme"}, open(TARGET, "w"))
