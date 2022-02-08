83a6e5d16fde2951e63026a073df77e3d50f25f2
123mjb <micahbutt@outlook.com>
2022-02-08 14:16:19 +0000

    tests

--- autogui.py
+++ autogui.py
@@ -1,8 +1,9 @@
 # importing modules
-import pyautogui
+import pyautogui,keyboard
 pyautogui.FAILSAFE=False
 # returns a size object with
 # width and height of the screen
 print(pyautogui.size())
+keyboard.wait('esc')
 pyautogui.click(x=1919, y=1079, clicks=1, interval=0, button='left')
 
---
+++ test.md
@@ -0,0 +1,4 @@
+# ***reasons you are sus***
+_number_ _fifteen_ _burgerking_ _foot_ _lettuce_
+# ***BEN IS SUSSY BAKA CONFIRMED***
+*"UwU"-Ben 2022* - ***This is sus***
