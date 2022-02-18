fc3fa60faf5c055b4285f3319b764280db5b6c17
123mjb <micahbutt@outlook.com>
2022-02-18 12:06:05 +0000

    testing

---
+++ Rick_Roll.py
@@ -0,0 +1,6 @@
+import webbrowser
+j=0
+while j!=200:
+    webbrowser.open("http://google.co.uk")
+    webbrowser.open("https://www.microsoft.com/en-gb/")
+    j+=1
--- autogui.py
+++ autogui.py
@@ -1,9 +1,22 @@
-# importing modules
+import subprocess,sys,os
+def install(package):
+  subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+install("pyautogui")
+install("keyboard")
 import pyautogui,keyboard
 pyautogui.FAILSAFE=False
-# returns a size object with
-# width and height of the screen
+
+def nothing():
+  hello=""
+def escape1():
+  pyautogui.click(x=1919, y=1079, clicks=1, interval=0, button='left')
+  sys.exit()
 print(pyautogui.size())
-keyboard.wait('esc')
-pyautogui.click(x=1919, y=1079, clicks=1, interval=0, button='left')
+# keyboard.wait('esc')
+keyboard.add_hotkey('Esc', lambda: escape1())
+while True:
+  nothing()
+
+  
+
 
